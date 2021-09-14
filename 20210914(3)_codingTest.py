cf619dde59e742c99938eebc417f50cb28969ebc
daje0601 <73736988+daje0601@users.noreply.github.com>
2021-09-14 12:13:40 +0900

    merge_list

---
+++ 20210914(3)_codingTest.py
@@ -0,0 +1,57 @@
+# leetcode 21. merge_list 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # 일단 두개의 링크드 리스트를 일반 리스트로 나는 변경을 해주었다. 
+        list1, list2 = [], []
+        
+        node = l1
+        while node != None:
+            list1.append(node.val)
+            node = node.next
+    
+        node2 = l2
+        while node2 != None:
+            list2.append(node2.val)
+            node2 = node2.next
+        
+        # 일반 리스트로 변경된 두개의 리스트를 더하고 정렬을 해준 후 
+        merge_list = sorted(list1+list2)
+        
+        # 다시 링크드 리스트로 변경하여 제출 답안 형식으로 변경한다. 
+        def lst2link(merge_list):
+            cur = dummy = ListNode(0)
+            for e in merge_list:
+                cur.next = ListNode(e)
+                cur = cur.next
+            return dummy.next
+        
+        return lst2link(merge_list)
+      
+## ------------------------------------------------------
+
+# solution1 - discussion 참조 
+def mergeTwoLists(self, a, b):
+    if not a or b and a.val > b.val:
+        a, b = b, a
+    if a:
+        a.next = self.mergeTwoLists(a.next, b)
+    return a
+  
+## ------------------------------------------------------
+
+# solution2 - discussion 참조 
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
