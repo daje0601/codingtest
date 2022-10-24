39d6345b99701b3e0584833c8670066b989674ed
daje0601 <73736988+daje0601@users.noreply.github.com>
2022-10-24 15:49:08 +0900

    238

--- leetcode/20210915_codingTest.py
+++ leetcode/20210915_codingTest.py
@@ -42,3 +42,17 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
 		p = p * nums[i]
 
 	return  answer
+
+# solution3
+def productExceptSelf(nums):
+        res = [1] * len(nums)   # res => [1 , 1, 1, 1]
+        prefix = 1 
+        postfix = 1
+    
+        for i in range(len(nums)) :  # [1,1,2,6], 24
+            res[i] = prefix
+            prefix *= nums[i]
+        for i in range(len(nums) - 1 , - 1 , - 1) :  # [24,24,12,4]
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
