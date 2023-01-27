9280c120511e08aaefc05d148eb7847cad4ac3b8
daje0601 <73736988+daje0601@users.noreply.github.com>
2023-01-27 14:38:49 +0900

    238. Product of Array Except Self (자신을 제외한 배열의 곱)

---
+++ leetcode/20230127(2).py
@@ -0,0 +1,12 @@
+#link : https://leetcode.com/problems/product-of-array-except-self/description/
+
+
+class Solution:
+    def productExceptSelf(self, nums):
+        ans, suf, pre = [1]*len(nums), 1, 1
+        for i in range(len(nums)):
+            ans[i] *= pre            
+            pre *= nums[i]
+            ans[-1-i] *= suf
+            suf *= nums[-1-i]
+        return ans
