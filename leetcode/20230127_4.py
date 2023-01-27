9a54aed649f492e4a945020b6f038b1e8c19f610
daje0601 <73736988+daje0601@users.noreply.github.com>
2023-01-27 23:24:28 +0900

    234. palindrome linked list(팰린드롬 연결 리스트)

---
+++ leetcode/20230127_4.py
@@ -0,0 +1,51 @@
+# link : https://leetcode.com/problems/palindrome-linked-list/solutions/?q=python&orderBy=most_votes
+
+# solution1
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        lst = [] 
+        while head :
+            lst.append(head.val)
+            head = head.next
+        
+        if lst == lst[::-1]:
+            return True
+          
+# solution2 
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        lst = [] 
+        while head :
+            lst.append(head.val)
+            head = head.next
+        
+        while len(lst) > 1 : 
+            if lst.pop(0) != lst.pop():
+                return False 
+        return True
+
+# solution3
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        lst = collections.deque()
+        while head :
+            lst.append(head.val)
+            head = head.next
+        
+        while len(lst) > 1 : 
+            if lst.pop(0) != lst.pop():
+                return False 
+        return True
+         
